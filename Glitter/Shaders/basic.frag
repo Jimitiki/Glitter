d39eb704da79181da4301c3759b0d55af417b24d
Jimitiki <cwjohnson42@gmail.com>
2017-10-05 20:49:47 -0600

    This better freakin work

--- Glitter/Shaders/basic.frag
+++ Glitter/Shaders/basic.frag
@@ -1,12 +1,9 @@
 #version 330 core
 in vec4 position;
-in vec4 color;
-
-uniform mat4 colorMask;
 
 out vec4 fColor;
 
 void main()
 {
-    fColor = colorMask * color;
+    fColor = vec4(1.0f, 1.0f, 1.0f, 1.0f);
 }
--- Glitter/Sources/main.cpp
+++ Glitter/Sources/main.cpp
@@ -49,34 +49,62 @@ int main(int argc, char * argv[])
 	glGenVertexArrays(1, &vao);
 	glBindVertexArray(vao);
 
-	float positions[] = {
-		-0.25f, -0.3f, 0.0f,
-		0.25f, -0.3f, 0.0f,
-		0.25f, 0.1f, 0.0f,
-		0.0f, 0.4f, 0.0f,
-		-0.25f, 0.1f, 0.0f
-	};
+	mlModel crayonModel;
+	if (!LoadModel("D:/Users/Chris/Documents/CS455/Lab4/", "crayon.obj", crayonModel))
+	{
+		return -1;
+	}
 
-	float colors[] = {
-		0.2f, 0.2f, 0.2f,
-		0.5f, 0.7f, 0.7f,
-		0.6f, 0.6f, 0.6f,
-		0.8f, 0.8f, 0.8f,
-		0.9f, 1.0f, 0.2f
-	};
+	GLuint shader = LoadProgram("D:/Users/Chris/Documents/Visual Studio 2017/Projects/Glitter/Glitter/Shaders/basic.vert",
+		"D:/Users/Chris/Documents/Visual Studio 2017/Projects/Glitter/Glitter/Shaders/basic.frag");
+
+	mlMesh crayonMesh = crayonModel.meshes[0];
 
 	GLuint vbo;
 	glGenBuffers(1, &vbo);
 	glBindBuffer(GL_ARRAY_BUFFER, vbo);
 
-	glBufferData(GL_ARRAY_BUFFER, sizeof(positions) + sizeof(colors),
-		nullptr, GL_STATIC_DRAW);
+	float positions[1584 * 3];
+	for (int i = 0; i < crayonMesh.vertices.size(); i++)
+	{
+		int pIndex = i * 3;
+		positions[pIndex] = crayonModel.meshes[0].vertices[i].position.x;
+		positions[pIndex + 1] = crayonModel.meshes[0].vertices[i].position.y;
+		positions[pIndex + 2] = crayonModel.meshes[0].vertices[i].position.z;
+	}
+
+	float UVs[1584 * 2];
+	for (int i = 0; i < crayonMesh.vertices.size(); i++)
+	{
+		int uIndex = i * 2;
+		UVs[uIndex] = crayonMesh.vertices[i].UV.x;
+		UVs[uIndex + 1] = crayonMesh.vertices[i].UV.y;
+	}
+
+	//glBufferData(GL_ARRAY_BUFFER, sizeof(crayonMesh.vertices), (GLvoid *) crayonMesh.vertices.data(), GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(positions) + sizeof(UVs), nullptr, GL_STATIC_DRAW);
 	glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(positions), positions);
-	glBufferSubData(GL_ARRAY_BUFFER, sizeof(positions), sizeof(colors), colors);
+	glBufferSubData(GL_ARRAY_BUFFER, sizeof(positions), sizeof(UVs), UVs);
 
 
-	GLuint shader = LoadProgram("D:/Users/Chris/Documents/Visual Studio 2017/Projects/Glitter/Glitter/Shaders/basic.vert",
-		"D:/Users/Chris/Documents/Visual Studio 2017/Projects/Glitter/Glitter/Shaders/basic.frag");
+	GLuint vPosition = glGetAttribLocation(shader, "vPosition");
+	glEnableVertexAttribArray(vPosition);
+	glVertexAttribPointer(vPosition, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GL_FLOAT), (GLvoid *) (nullptr));
+
+	GLuint vUV = glGetAttribLocation(shader, "vUV");
+	glEnableVertexAttribArray(vPosition);
+	glVertexAttribPointer(vUV, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(GL_FLOAT), (GLvoid *) sizeof(positions));
+
+	glm::mat4 p = glm::perspective(glm::radians(90.0f), (float) mWidth / (float) mHeight, 0.01f, 100.0f);
+	GLint perspectivePositionID = glGetUniformLocation(shader, "perspective");
+
+	glm::mat4 t = matrixTransform(glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(1.0f, 1.0f, 1.0f));
+	GLint transformPositionID = glGetUniformLocation(shader, "transform");
+
+	//t = p * t;
+
+	glm::vec4 v1 = p * t * glm::vec4(1.0f, 1.0f, 0.0f, 1.0f);
+	glm::vec4 v2 = p * t * glm::vec4(-0.2f, -0.2f, -0.2f, 1.0f);
 
 	// Rendering Loop
 	while (glfwWindowShouldClose(mWindow) == false)
@@ -88,61 +116,16 @@ int main(int argc, char * argv[])
 		glClear(GL_COLOR_BUFFER_BIT);
 		glUseProgram(shader);
 
-		glBindVertexArray(vao);
-		GLuint vPosition = glGetAttribLocation(shader, "vPosition");
-		glEnableVertexAttribArray(vPosition);
-		glVertexAttribPointer(vPosition, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GL_FLOAT), (GLvoid *) (nullptr));
-
-		GLuint vColor = glGetAttribLocation(shader, "vColor");
-		glEnableVertexAttribArray(vColor);
-		glVertexAttribPointer(vColor, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GL_FLOAT), (GLvoid *) (sizeof(positions)));
-
-		glm::mat4 t = matrixTransform(glm::vec3(0.3f, -0.3f, 0.0f), glm::vec3(0.9f, 0.9f, 0.0f));
-
-		GLint transformPositionID = glGetUniformLocation(shader, "transform");
 		glUniformMatrix4fv(transformPositionID, 1, GL_FALSE, &t[0][0]);
 
-		GLint colorPositionID = glGetUniformLocation(shader, "colorMask");
-		
-		glm::mat4 c = glm::mat4(
-			1.0f, 0.0f, 0.0f, 0.0f,
-			0.0f, 0.6f, 0.0f, 0.0f,
-			0.0f, 0.0f, 0.6f, 0.0f,
-			0.0f, 0.0f, 0.0f, 1.0f
-		);
-		glUniformMatrix4fv(colorPositionID, 1, GL_FALSE, &c[0][0]);
-
-		glDrawArrays(GL_TRIANGLE_FAN, 0, 5);
-
-		t = matrixTransform(glm::vec3(0.1f, 0.1f, 0.0f), glm::vec3(1.2f, 1.2f, 0.0f));
-		glUniformMatrix4fv(transformPositionID, 1, GL_FALSE, &t[0][0]);
-		c = glm::mat4(
-				0.0f, 0.0f, 0.0f, 0.0f,
-				0.0f, 1.0f, 0.0f, 0.0f,
-				0.0f, 0.0f, 1.0f, 0.0f,
-				0.0f, 0.0f, 0.0f, 1.0f
-		);
-		glUniformMatrix4fv(colorPositionID, 1, GL_FALSE, &c[0][0]);
-
-		glDrawArrays(GL_TRIANGLE_FAN, 0, 5);
-
-		t = matrixTransform(glm::vec3(-0.4f, 0.6f, 0.0f), glm::vec3(0.6f, 0.6f, 0.0f));
-		glUniformMatrix4fv(transformPositionID, 1, GL_FALSE, &t[0][0]);
-		c = glm::mat4(
-			0.7f, 0.0f, 0.0f, 0.0f,
-			0.0f, 0.6f, 0.0f, 0.0f,
-			0.0f, 0.0f, 0.8f, 0.0f,
-			0.0f, 0.0f, 0.0f, 1.0f
-		);
-		glUniformMatrix4fv(colorPositionID, 1, GL_FALSE, &c[0][0]);
-
-		glDrawArrays(GL_TRIANGLE_FAN, 0, 5);
+		glDrawArrays(GL_TRIANGLES, 0, 1584);
 		
 
 		// Flip Buffers and Draw
 		glfwSwapBuffers(mWindow);
 		glfwPollEvents();
-	}  glfwTerminate();
+	}
+	glfwTerminate();
 
 	return EXIT_SUCCESS;
 }
